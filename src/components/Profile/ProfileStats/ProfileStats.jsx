8f28e37c16da1c1709f7e79d193d3ceca7b84137
Ceres Rasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-04-08 23:56:57 +0300

    task-1 done

--- src/components/Profile/ProfileStats/ProfileStats.jsx
+++ src/components/Profile/ProfileStats/ProfileStats.jsx
@@ -4,17 +4,17 @@ import css from 'components/Profile/ProfileStats/ProfileStats.module.css';
 export const ProfileStats = ({ stats: { followers, views, likes } }) => {
   return (
     <ul className={css.stats}>
-      <li>
-        <span className={css.stats}>Followers</span>
-        <span className={css.stats}>{followers}</span>
+      <li className={css.stats_list}>
+        <span className={css.stats_title}>Followers</span>
+        <span className={css.stats_value}>{followers}</span>
       </li>
-      <li>
-        <span className={css.stats}>Views</span>
-        <span className={css.stats}>{views}</span>
+      <li className={css.stats_list}>
+        <span className={css.stats_title}>Views</span>
+        <span className={css.stats_value}>{views}</span>
       </li>
-      <li>
-        <span className={css.stats}>Likes</span>
-        <span className={css.stats}>{likes}</span>
+      <li className={css.stats_list}>
+        <span className={css.stats_title}>Likes</span>
+        <span className={css.stats_value}>{likes}</span>
       </li>
     </ul>
   );
